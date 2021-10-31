605f89cd81e082d36648a99701458349a58dcad5
Omar Hegazy <Omar.Hegazy@intelsoftsystems.com>
2021-10-31 04:06:31 +0200

    added state in burger builder - pass it to Burger + transformed props object to array + display transformed ingredients

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react'
 import Aux from '../../hoc/Auxiliraly'
 
-
+import Burger from '../../components/Burger/Burger.jsx';
 
 class BurgerBuilder extends Component {
+
+    state = {
+        ingredents: {
+            salad: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 0
+        }
+    }
+
     render() {
         return (
             <Aux>
-                <div>Burger</div>
+                <Burger ingredients={this.state.ingredents} />
                 <div>Build Controls</div>
             </Aux>
         )
