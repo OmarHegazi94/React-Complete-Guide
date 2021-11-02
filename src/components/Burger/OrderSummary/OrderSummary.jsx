f5baeacb60eac36cbcee0d8407ff6309e6887e57
Omar Hegazy <Omar.Hegazy@intelsoftsystems.com>
2021-11-02 16:24:45 +0200

    added modal - backdrop - button components

---
+++ src/components/Burger/OrderSummary/OrderSummary.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+// import Aux from "../../../hoc/Auxiliraly";
+import Button from "../../UI/Button/Button.jsx";
+
+function OrderSummary(props) {
+  const ingredientSummary = Object.keys(props.ingredients).map((ingredientKey) => {
+    return (
+      <li key={ingredientKey}>
+        <span style={{textTransform: "capitalize"}}>{ingredientKey}</span> {props.ingredients[ingredientKey]}
+      </li>
+    );
+  });
+
+  return (
+    <>
+      <h3>Your order</h3>
+      <p>A delicious burger with the following ingredients:</p>
+      <ul>{ingredientSummary}</ul>
+      <p>Continue to checkout?</p>
+
+      <Button btnType="Danger" clicked={props.purchaseCanceled}>
+        Cancel
+      </Button>
+      <Button btnType="Success" clicked={props.purchaseContinue}>
+        Continue
+      </Button>
+    </>
+  );
+}
+
+export default OrderSummary;
